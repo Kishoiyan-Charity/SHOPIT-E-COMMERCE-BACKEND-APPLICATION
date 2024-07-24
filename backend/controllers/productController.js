248f5d8b034f92e82c4a8f63f955432796f395c1
Kishoiyan-Charity1 <charity.kishoiyancharity@gmail.com>
2024-07-24 15:52:57 +0300

    changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,18 @@
+const Product = require('../models/product')
+
+//CREATE NEW PRODUCT
+exports.newProduct = async (req, res, next) => {
+
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        product
+    })
+
+}
+
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
